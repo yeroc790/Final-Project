fd7fb2e7d6acd2bfb4d2d7a1fcd7838b825fb281
Corey's Desktop <spam4corey@gmail.com>
2017-05-02 10:48:06 -0500

    Boss Rename

--- DungeonCrawler/src/objects/monsters/boss/Azmodan.java
+++ DungeonCrawler/src/objects/monsters/boss/Azmodan.java
@@ -9,20 +9,20 @@ package objects.monsters.boss;
  *
  * @author Corey
  */
-public class Azmodan extends Boss{ //Azmodan, Hope's End
+public class Azmodan extends Boss{ //Azmodan the Spined
     private final int ATTACK = 40;
     private final int DEFENSE = 10;
     private final int HEALTH = 30;
 
     public Azmodan(int row, int col) {
-        super(row, col, "Azmodan, Hope's End");
+        super(row, col, "Azmodan the Spined");
         setAttack(ATTACK);
         setDefense(DEFENSE);
         setHealth(HEALTH);
     }
 
     public Azmodan(){
-        super(0,0,"Azmodan, Hope's End");
+        super(0,0,"Azmodan the Spined");
         setAttack(ATTACK);
         setDefense(DEFENSE);
         setHealth(HEALTH);
